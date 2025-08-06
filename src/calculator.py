55538dbffaa2d4696d295813f493f67283953c59
ori <ori123@gmail.com>
2025-08-06 17:06:55 +0300

    conflicting change in add function

--- src/calculator.py
+++ src/calculator.py
@@ -3,6 +3,7 @@ class Calculator:
     @staticmethod
     def add(a: float, b: float) -> float:
         """Add two numbers."""
+        print("adding numbers")
         return a + b
 
     @staticmethod
